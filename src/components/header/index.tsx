f52cd7e3b92b5ea33bb617af4ef0d52804b63a03
AHMET SARIDAG <ahmet.saridagj@gmail.com>
2024-05-28 18:25:43 +0300

    Project initiation, created by Next.js with TypeScript.

---
+++ src/components/header/index.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { BsFillRocketTakeoffFill } from "react-icons/bs";
+
+export default function Header() {
+  // create a navbarlist
+  const navbarList = [
+    {
+      path: "/",
+      text: "Pricing",
+    },
+    {
+      path: "/",
+      text: "Demo",
+    },
+    {
+      path: "/",
+      text: "Wall of love",
+    },
+    {
+      path: "/",
+      text: "Leaderboard",
+    },
+  ];
+
+  return (
+    <section className="container">
+      <div className="flex justify-between items-center p-5">
+        <a
+          href="/"
+          className="w-28 md:w-32 flex gap-1 justify-center items-center cursor-pointer"
+        >
+          <BsFillRocketTakeoffFill className="text-3xl text-primary" />
+          <h2 className="text-2xl font-semibold">JetStart</h2>
+        </a>
+        <div>
+          <ul className="flex gap-10">
+            {navbarList.map((item, idx) => (
+              <li className={idx > 1 ? "sm:block hidden" : ""} key={idx}>
+                <a
+                  href={item.path}
+                  className="hover:border-b-2 border-primary leading-6"
+                >
+                  {item.text}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,47 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      backgroundImage: {},
+      container: {
+        center: true,
+      },
+      colors: {
+        "hacker-news": "#FF6600",
+        "product-hunt": "#ff6052",
+        "x-logo": "#0f1419",
+        "reddit-color": "#ff4500",
       },
     },
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#FFD700",
+          "primary-content": "#131616",
+          secondary: "#50C878",
+          "secondary-content": "#FAFAFA",
+          accent: "#FFD700",
+          neutral: "#001015",
+          "base-content": "#cfcfcf",
+          "base-100": "#25292e",
+          info: "#0094ff",
+          success: "#00bc59",
+          warning: "#e9b600",
+          error: "#f90026",
+        },
+        // mytheme: {
+        //   primary: "#059669",
+        //   secondary: "#5c5b61",
+        //   "secondary-content": "#059669",
+        //   "base-100": "#cfcfcf",
+        //   "base-200": "#BEBEBE",
+        //   "base-300": "#A9A9A9",
+        //   "base-content": "#30313D",
+        // },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
 };
 export default config;
